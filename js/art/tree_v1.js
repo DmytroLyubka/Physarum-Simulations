eaa297d90b00827abbf0817824273d20131abc80
Dmytro Lyubka <dmytrolyubka@gmail.com>
2023-11-04 00:09:15 +0000

    Fractal tree v1 finished

--- js/art/tree_v1.js
+++ js/art/tree_v1.js
@@ -1,5 +1,5 @@
-const initialLength = 100
-const initialAngle = 45
+const initialLength = 200
+const initialAngle = 65
 
 function setup() {
     let canvasSize = 850
@@ -10,20 +10,23 @@ function setup() {
     angleMode(DEGREES)
 }
 
-const iterMax = 11
-
+/**
+ * Recursive function to generate fractal tree.
+ * @param {*} length 
+ * @param {*} angle 
+ */
 function branch(length, angle) {
     line(0, 0, 0, -length)
     translate(0, -length)
-    length *= 0.65
-    angle = random(angle - 15, angle + 15)
+    length *= 0.7 // removes 30% of branch length on each iteration
+    angle = random(angle - 25, angle + 25) // slightly randomizes branch angle
 
     if (length > 2)
     {
-        push()
+        push() // saves sketch settings before generating next branch
         rotate(angle)
         branch(length, angle)
-        pop()
+        pop() // restores sketch settings after branch has been generated
 
         push()
         rotate(-angle)
